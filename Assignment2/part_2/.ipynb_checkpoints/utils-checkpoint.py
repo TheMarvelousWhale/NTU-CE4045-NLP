428a853dee7d9f3ab2ab08ff15bfc784ff254e67
Hoang Viet <51878941+TheMarvelousWhale@users.noreply.github.com>
2021-11-03 02:25:29 +0800

    feat: refactor the code for multi archi run
    The code now support auto running of multiple architectures. Please read to uds

---
+++ Assignment2/part_2/.ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,555 @@
+from __future__ import print_function
+from collections import OrderedDict
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+from torch.nn import init
+from torch.autograd import Variable
+from torch import autograd
+
+import time,os,sys,codecs,re,urllib,math
+import _pickle as cPickle
+
+#Constants
+START_TAG = '<START>'
+STOP_TAG = '<STOP>'
+
+##############################  DATA PROCESSING   ###########################
+def zero_digits(s):
+    """
+    Replace every digit in a string by a zero.
+    """
+    return re.sub('\d', '0', s)
+
+def load_sentences(path, zeros):
+    """
+    Load sentences. A line must contain at least a word and its tag.
+    Sentences are separated by empty lines.
+    """
+    sentences = []
+    sentence = []
+    for line in codecs.open(path, 'r', 'utf8'):
+        line = zero_digits(line.rstrip()) if zeros else line.rstrip()
+        if not line:
+            if len(sentence) > 0:
+                if 'DOCSTART' not in sentence[0][0]:
+                    sentences.append(sentence)
+                sentence = []
+        else:
+            word = line.split()
+            assert len(word) >= 2
+            sentence.append(word)
+    if len(sentence) > 0:
+        if 'DOCSTART' not in sentence[0][0]:
+            sentences.append(sentence)
+    return sentences
+
+
+def iob2(tags):
+    """
+    Check that tags have a valid BIO format.
+    Tags in BIO1 format are converted to BIO2.
+    """
+    for i, tag in enumerate(tags):
+        if tag == 'O':
+            continue
+        split = tag.split('-')
+        if len(split) != 2 or split[0] not in ['I', 'B']:
+            return False
+        if split[0] == 'B':
+            continue
+        elif i == 0 or tags[i - 1] == 'O':  # conversion IOB1 to IOB2
+            tags[i] = 'B' + tag[1:]
+        elif tags[i - 1][1:] == tag[1:]:
+            continue
+        else:  # conversion IOB1 to IOB2
+            tags[i] = 'B' + tag[1:]
+    return True
+
+def iob_iobes(tags):
+    """
+    the function is used to convert
+    BIO -> BIOES tagging
+    """
+    new_tags = []
+    for i, tag in enumerate(tags):
+        if tag == 'O':
+            new_tags.append(tag)
+        elif tag.split('-')[0] == 'B':
+            if i + 1 != len(tags) and \
+               tags[i + 1].split('-')[0] == 'I':
+                new_tags.append(tag)
+            else:
+                new_tags.append(tag.replace('B-', 'S-'))
+        elif tag.split('-')[0] == 'I':
+            if i + 1 < len(tags) and \
+                    tags[i + 1].split('-')[0] == 'I':
+                new_tags.append(tag)
+            else:
+                new_tags.append(tag.replace('I-', 'E-'))
+        else:
+            raise Exception('Invalid IOB format!')
+    return new_tags
+
+def update_tag_scheme(sentences, tag_scheme):
+    """
+    Check and update sentences tagging scheme to BIO2
+    Only BIO1 and BIO2 schemes are accepted for input data.
+    """
+    for i, s in enumerate(sentences):
+        tags = [w[-1] for w in s]
+        # Check that tags are given in the BIO format
+        if not iob2(tags):
+            s_str = '\n'.join(' '.join(w) for w in s)
+            raise Exception('Sentences should be given in BIO format! ' +
+                            'Please check sentence %i:\n%s' % (i, s_str))
+        if tag_scheme == 'BIOES':
+            new_tags = iob_iobes(tags)
+            for word, new_tag in zip(s, new_tags):
+                word[-1] = new_tag
+        else:
+            raise Exception('Wrong tagging scheme!')
+            
+            
+def create_dico(item_list):
+    """
+    Create a dictionary of items from a list of list of items.
+    """
+    assert type(item_list) is list
+    dico = {}
+    for items in item_list:
+        for item in items:
+            if item not in dico:
+                dico[item] = 1
+            else:
+                dico[item] += 1
+    return dico
+
+def create_mapping(dico):
+    """
+    Create a mapping (item to ID / ID to item) from a dictionary.
+    Items are ordered by decreasing frequency.
+    """
+    sorted_items = sorted(dico.items(), key=lambda x: (-x[1], x[0]))
+    id_to_item = {i: v[0] for i, v in enumerate(sorted_items)}
+    item_to_id = {v: k for k, v in id_to_item.items()}
+    return item_to_id, id_to_item
+
+def word_mapping(sentences, lower):
+    """
+    Create a dictionary and a mapping of words, sorted by frequency.
+    """
+    words = [[x[0].lower() if lower else x[0] for x in s] for s in sentences]
+    dico = create_dico(words)
+    dico['<UNK>'] = 10000000 #UNK tag for unknown words
+    word_to_id, id_to_word = create_mapping(dico)
+    print("Found %i unique words (%i in total)" % (
+        len(dico), sum(len(x) for x in words)
+    ))
+    return dico, word_to_id, id_to_word
+
+def char_mapping(sentences):
+    """
+    Create a dictionary and mapping of characters, sorted by frequency.
+    """
+    chars = ["".join([w[0] for w in s]) for s in sentences]
+    dico = create_dico(chars)
+    char_to_id, id_to_char = create_mapping(dico)
+    print("Found %i unique characters" % len(dico))
+    return dico, char_to_id, id_to_char
+
+def tag_mapping(sentences):
+    """
+    Create a dictionary and a mapping of tags, sorted by frequency.
+    """
+    tags = [[word[-1] for word in s] for s in sentences]
+    dico = create_dico(tags)
+    dico[START_TAG] = -1
+    dico[STOP_TAG] = -2
+    tag_to_id, id_to_tag = create_mapping(dico)
+    print("Found %i unique named entity tags" % len(dico))
+    return dico, tag_to_id, id_to_tag
+
+
+def lower_case(x,lower=False):
+    if lower:
+        return x.lower()  
+    else:
+        return x
+    
+    
+def prepare_dataset(sentences, word_to_id, char_to_id, tag_to_id, lower=False):
+    """
+    Prepare the dataset. Return a list of lists of dictionaries containing:
+        - word indexes
+        - word char indexes
+        - tag indexes
+    """
+    data = []
+    for s in sentences:
+        str_words = [w[0] for w in s]
+        words = [word_to_id[lower_case(w,lower) if lower_case(w,lower) in word_to_id else '<UNK>']
+                 for w in str_words]
+        # Skip characters that are not in the training set
+        chars = [[char_to_id[c] for c in w if c in char_to_id]
+                 for w in str_words]
+        tags = [tag_to_id[w[-1]] for w in s]
+        data.append({
+            'str_words': str_words,
+            'words': words,
+            'chars': chars,
+            'tags': tags,
+        })
+    return data
+
+
+######################    MODEL TRAINING    ####################
+
+def init_embedding(input_embedding):
+    """
+    Initialize embedding
+    """
+    bias = np.sqrt(3.0 / input_embedding.size(1))
+    nn.init.uniform_(input_embedding, -bias, bias)
+    
+def init_linear(input_linear):
+    """
+    Initialize linear transformation
+    """
+    bias = np.sqrt(6.0 / (input_linear.weight.size(0) + input_linear.weight.size(1)))
+    nn.init.uniform_(input_linear.weight, -bias, bias)
+    if input_linear.bias is not None:
+        input_linear.bias.data.zero_()
+        
+        
+def init_lstm(input_lstm):
+    """
+    Initialize lstm
+    
+    PyTorch weights parameters:
+    
+        weight_ih_l[k]: the learnable input-hidden weights of the k-th layer,
+            of shape `(hidden_size * input_size)` for `k = 0`. Otherwise, the shape is
+            `(hidden_size * hidden_size)`
+            
+        weight_hh_l[k]: the learnable hidden-hidden weights of the k-th layer,
+            of shape `(hidden_size * hidden_size)`            
+    """
+    
+    # Weights init for forward layer
+    for ind in range(0, input_lstm.num_layers):
+        
+        ## Gets the weights Tensor from our model, for the input-hidden weights in our current layer
+        weight = eval('input_lstm.weight_ih_l' + str(ind))
+        
+        # Initialize the sampling range
+        sampling_range = np.sqrt(6.0 / (weight.size(0) / 4 + weight.size(1)))
+        
+        # Randomly sample from our samping range using uniform distribution and apply it to our current layer
+        nn.init.uniform_(weight, -sampling_range, sampling_range)
+        
+        # Similar to above but for the hidden-hidden weights of the current layer
+        weight = eval('input_lstm.weight_hh_l' + str(ind))
+        sampling_range = np.sqrt(6.0 / (weight.size(0) / 4 + weight.size(1)))
+        nn.init.uniform_(weight, -sampling_range, sampling_range)
+        
+        
+    # We do the above again, for the backward layer if we are using a bi-directional LSTM (our final model uses this)
+    if input_lstm.bidirectional:
+        for ind in range(0, input_lstm.num_layers):
+            weight = eval('input_lstm.weight_ih_l' + str(ind) + '_reverse')
+            sampling_range = np.sqrt(6.0 / (weight.size(0) / 4 + weight.size(1)))
+            nn.init.uniform_(weight, -sampling_range, sampling_range)
+            weight = eval('input_lstm.weight_hh_l' + str(ind) + '_reverse')
+            sampling_range = np.sqrt(6.0 / (weight.size(0) / 4 + weight.size(1)))
+            nn.init.uniform_(weight, -sampling_range, sampling_range)
+
+    # Bias initialization steps
+    
+    # We initialize them to zero except for the forget gate bias, which is initialized to 1
+    if input_lstm.bias:
+        for ind in range(0, input_lstm.num_layers):
+            bias = eval('input_lstm.bias_ih_l' + str(ind))
+            
+            # Initializing to zero
+            bias.data.zero_()
+            
+            # This is the range of indices for our forget gates for each LSTM cell
+            bias.data[input_lstm.hidden_size: 2 * input_lstm.hidden_size] = 1
+            
+            #Similar for the hidden-hidden layer
+            bias = eval('input_lstm.bias_hh_l' + str(ind))
+            bias.data.zero_()
+            bias.data[input_lstm.hidden_size: 2 * input_lstm.hidden_size] = 1
+            
+        # Similar to above, we do for backward layer if we are using a bi-directional LSTM 
+        if input_lstm.bidirectional:
+            for ind in range(0, input_lstm.num_layers):
+                bias = eval('input_lstm.bias_ih_l' + str(ind) + '_reverse')
+                bias.data.zero_()
+                bias.data[input_lstm.hidden_size: 2 * input_lstm.hidden_size] = 1
+                bias = eval('input_lstm.bias_hh_l' + str(ind) + '_reverse')
+                bias.data.zero_()
+                bias.data[input_lstm.hidden_size: 2 * input_lstm.hidden_size] = 1
+                
+                
+def log_sum_exp(vec):
+    '''
+    This function calculates the score explained above for the forward algorithm
+    vec 2D: 1 * tagset_size
+    '''
+    max_score = vec[0, argmax(vec)]
+    max_score_broadcast = max_score.view(1, -1).expand(1, vec.size()[1])
+    return max_score + torch.log(torch.sum(torch.exp(vec - max_score_broadcast)))
+    
+def argmax(vec):
+    '''
+    This function returns the max index in a vector
+    '''
+    _, idx = torch.max(vec, 1)
+    return to_scalar(idx)
+
+def to_scalar(var):
+    '''
+    Function to convert pytorch tensor to a scalar
+    '''
+    return var.view(-1).data.tolist()[0]
+
+
+def score_sentences(self, feats, tags):
+    # tags is ground_truth, a list of ints, length is len(sentence)
+    # feats is a 2D tensor, len(sentence) * tagset_size
+    r = torch.LongTensor(range(feats.size()[0]))
+    if self.use_gpu:
+        r = r.cuda()
+        pad_start_tags = torch.cat([torch.cuda.LongTensor([self.tag_to_ix[START_TAG]]), tags])
+        pad_stop_tags = torch.cat([tags, torch.cuda.LongTensor([self.tag_to_ix[STOP_TAG]])])
+    else:
+        pad_start_tags = torch.cat([torch.LongTensor([self.tag_to_ix[START_TAG]]), tags])
+        pad_stop_tags = torch.cat([tags, torch.LongTensor([self.tag_to_ix[STOP_TAG]])])
+
+    score = torch.sum(self.transitions[pad_stop_tags, pad_start_tags]) + torch.sum(feats[r, tags])
+
+    return score
+
+
+def forward_alg(self, feats):
+    '''
+    This function performs the forward algorithm explained above
+    '''
+    # calculate in log domain
+    # feats is len(sentence) * tagset_size
+    # initialize alpha with a Tensor with values all equal to -10000.
+    
+    # Do the forward algorithm to compute the partition function
+    init_alphas = torch.Tensor(1, self.tagset_size).fill_(-10000.)
+    
+    # START_TAG has all of the score.
+    init_alphas[0][self.tag_to_ix[START_TAG]] = 0.
+    
+    # Wrap in a variable so that we will get automatic backprop
+    forward_var = autograd.Variable(init_alphas)
+    if self.use_gpu:
+        forward_var = forward_var.cuda()
+        
+    # Iterate through the sentence
+    for feat in feats:
+        # broadcast the emission score: it is the same regardless of
+        # the previous tag
+        emit_score = feat.view(-1, 1)
+        
+        # the ith entry of trans_score is the score of transitioning to
+        # next_tag from i
+        tag_var = forward_var + self.transitions + emit_score
+        
+        # The ith entry of next_tag_var is the value for the
+        # edge (i -> next_tag) before we do log-sum-exp
+        max_tag_var, _ = torch.max(tag_var, dim=1)
+        
+        # The forward variable for this tag is log-sum-exp of all the
+        # scores.
+        tag_var = tag_var - max_tag_var.view(-1, 1)
+        
+        # Compute log sum exp in a numerically stable way for the forward algorithm
+        forward_var = max_tag_var + torch.log(torch.sum(torch.exp(tag_var), dim=1)).view(1, -1) # ).view(1, -1)
+    terminal_var = (forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]).view(1, -1)
+    alpha = log_sum_exp(terminal_var)
+    # Z(x)
+    return alpha
+
+def viterbi_algo(self, feats):
+    '''
+    In this function, we implement the viterbi algorithm explained above.
+    A Dynamic programming based approach to find the best tag sequence
+    '''
+    backpointers = []
+    # analogous to forward
+    
+    # Initialize the viterbi variables in log space
+    init_vvars = torch.Tensor(1, self.tagset_size).fill_(-10000.)
+    init_vvars[0][self.tag_to_ix[START_TAG]] = 0
+    
+    # forward_var at step i holds the viterbi variables for step i-1
+    forward_var = Variable(init_vvars)
+    if self.use_gpu:
+        forward_var = forward_var.cuda()
+    for feat in feats:
+        next_tag_var = forward_var.view(1, -1).expand(self.tagset_size, self.tagset_size) + self.transitions
+        _, bptrs_t = torch.max(next_tag_var, dim=1)
+        bptrs_t = bptrs_t.squeeze().data.cpu().numpy() # holds the backpointers for this step
+        next_tag_var = next_tag_var.data.cpu().numpy() 
+        viterbivars_t = next_tag_var[range(len(bptrs_t)), bptrs_t] # holds the viterbi variables for this step
+        viterbivars_t = Variable(torch.FloatTensor(viterbivars_t))
+        if self.use_gpu:
+            viterbivars_t = viterbivars_t.cuda()
+            
+        # Now add in the emission scores, and assign forward_var to the set
+        # of viterbi variables we just computed
+        forward_var = viterbivars_t + feat
+        backpointers.append(bptrs_t)
+
+    # Transition to STOP_TAG
+    terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]
+    terminal_var.data[self.tag_to_ix[STOP_TAG]] = -10000.
+    terminal_var.data[self.tag_to_ix[START_TAG]] = -10000.
+    best_tag_id = argmax(terminal_var.unsqueeze(0))
+    path_score = terminal_var[best_tag_id]
+    
+    # Follow the back pointers to decode the best path.
+    best_path = [best_tag_id]
+    for bptrs_t in reversed(backpointers):
+        best_tag_id = bptrs_t[best_tag_id]
+        best_path.append(best_tag_id)
+        
+    # Pop off the start tag (we dont want to return that to the caller)
+    start = best_path.pop()
+    assert start == self.tag_to_ix[START_TAG] # Sanity check
+    best_path.reverse()
+    return path_score, best_path
+
+def forward_calc(self, sentence, chars, chars2_length, d,cnn_archi):
+    
+    '''
+    The function calls viterbi decode and generates the 
+    most probable sequence of tags for the sentence
+    '''
+    
+    # Get the emission scores from the BiLSTM
+    feats = self._get_lstm_features(sentence, chars, chars2_length, d,cnn_archi)
+    # viterbi to get tag_seq
+    
+    # Find the best path, given the features.
+    if self.use_crf:
+        score, tag_seq = self.viterbi_decode(feats)
+    else:
+        score, tag_seq = torch.max(feats, 1)
+        tag_seq = list(tag_seq.cpu().data)
+
+    return score, tag_seq
+
+
+def get_neg_log_likelihood(self, sentence, tags, chars2, chars2_length, d,cnn_archi):
+    # sentence, tags is a list of ints
+    # features is a 2D tensor, len(sentence) * self.tagset_size
+    feats = self._get_lstm_features(sentence, chars2, chars2_length, d,cnn_archi)
+
+    if self.use_crf:
+        forward_score = self._forward_alg(feats)
+        gold_score = self._score_sentence(feats, tags)
+        return forward_score - gold_score
+    else:
+        tags = Variable(tags)
+        scores = nn.functional.cross_entropy(feats, tags)
+        return scores
+    
+    
+##################### EVALUATION HELPERS #########################################
+
+def get_chunk_type(tok, idx_to_tag):
+    """
+    The function takes in a chunk ("B-PER") and then splits it into the tag (PER) and its class (B)
+    as defined in BIOES
+    
+    Args:
+        tok: id of token, ex 4
+        idx_to_tag: dictionary {4: "B-PER", ...}
+
+    Returns:
+        tuple: "B", "PER"
+
+    """
+    
+    tag_name = idx_to_tag[tok]
+    tag_class = tag_name.split('-')[0]
+    tag_type = tag_name.split('-')[-1]
+    return tag_class, tag_type
+
+
+def get_chunks(seq, tags):
+    """Given a sequence of tags, group entities and their position
+
+    Args:
+        seq: [4, 4, 0, 0, ...] sequence of labels
+        tags: dict["O"] = 4
+
+    Returns:
+        list of (chunk_type, chunk_start, chunk_end)
+
+    Example:
+        seq = [4, 5, 0, 3]
+        tags = {"B-PER": 4, "I-PER": 5, "B-LOC": 3}
+        result = [("PER", 0, 2), ("LOC", 3, 4)]
+
+    """
+    
+    # We assume by default the tags lie outside a named entity
+    default = tags["O"]
+    
+    idx_to_tag = {idx: tag for tag, idx in tags.items()}
+    
+    chunks = []
+    
+    chunk_type, chunk_start = None, None
+    for i, tok in enumerate(seq):
+        # End of a chunk 1
+        if tok == default and chunk_type is not None:
+            # Add a chunk.
+            chunk = (chunk_type, chunk_start, i)
+            chunks.append(chunk)
+            chunk_type, chunk_start = None, None
+
+        # End of a chunk + start of a chunk!
+        elif tok != default:
+            tok_chunk_class, tok_chunk_type = get_chunk_type(tok, idx_to_tag)
+            if chunk_type is None:
+                # Initialize chunk for each entity
+                chunk_type, chunk_start = tok_chunk_type, i
+            elif tok_chunk_type != chunk_type or tok_chunk_class == "B":
+                # If chunk class is B, i.e., its a beginning of a new named entity
+                # or, if the chunk type is different from the previous one, then we
+                # start labelling it as a new entity
+                chunk = (chunk_type, chunk_start, i)
+                chunks.append(chunk)
+                chunk_type, chunk_start = tok_chunk_type, i
+        else:
+            pass
+
+    # end condition
+    if chunk_type is not None:
+        chunk = (chunk_type, chunk_start, len(seq))
+        chunks.append(chunk)
+
+    return chunks
+
+
+
+
+
+def adjust_learning_rate(optimizer, lr):
+    """
+    shrink learning rate
+    """
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
