5ff8bae98f080ddd5a50b6d66fe343bd70b7d664
Hoang Viet <51878941+TheMarvelousWhale@users.noreply.github.com>
2021-11-04 21:19:08 +0800

    Initial commit

--- Assignment2/part_1/data_fnn.py
+++ Assignment2/part_1/data_fnn.py
@@ -7,11 +7,15 @@ class Dictionary(object):
     def __init__(self):
         self.word2idx = {}
         self.idx2word = []
+        self.word2count = {}
 
     def add_word(self, word):
         if word not in self.word2idx:
             self.idx2word.append(word)
             self.word2idx[word] = len(self.idx2word) - 1
+            self.word2count[word] = 1
+        else:
+            self.word2count[word] +=1
         return self.word2idx[word]
 
     def __len__(self):
@@ -26,33 +30,24 @@ class Corpus(object):
         self.test = self.tokenize(os.path.join(path, 'test.txt'))
     
     def process_line(self, line):
-        return re.sub(r"[^a-zA-Z0-9,\.!?<>\- ]+", '', line)
+        return re.sub(r"[^a-zA-Z0-9,\.!?<>\- ]+", '', line).replace('<unk>','').lower()
 
     def tokenize(self, path):
         """Tokenizes a text file."""
         assert os.path.exists(path)
-        # Add words to the dictionary
+        
+        idss = []
+        # Add words to the dictionary and tokenize
         with open(path, 'r', encoding="utf8") as f:
             for line in f:
                 line = self.process_line(line)
-                words = line.split() + ['<eos>']
-                if words[0]=='<eos>':
+                if line == []:
                     continue
+                words = line.split() 
+                ids = []
                 for word in words:
                     self.dictionary.add_word(word)
-
-        # Tokenize file content
-        with open(path, 'r', encoding="utf8") as f:
-            idss = []
-            for line in f:
-                line = self.process_line(line)
-                words = line.split() + ['<eos>']
-                ids = []
-                if words[0]=='<eos>':
-                    continue
-                for word in words:
                     ids.append(self.dictionary.word2idx[word])
                 idss.append(torch.tensor(ids).type(torch.int64))
             ids = torch.cat(idss)
-
         return ids
